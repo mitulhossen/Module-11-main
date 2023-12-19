61ce63f117e00f1f6dc5da6b910936f1c814c5e3
Md. Mitul Hossen <143118872+mitulhossen@users.noreply.github.com>
2023-12-19 23:28:59 +0600

    Update footer.blade.php

--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -6,7 +6,7 @@
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
-                    Design & Develop by RAMK Soft Tech
+                    Design & Develop by MAMK Soft Tech
                 </div>
             </div>
         </div>
