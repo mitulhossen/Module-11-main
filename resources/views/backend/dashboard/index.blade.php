5185f8fa352a3c10358d5acaaabe85e47c5c58f3
Md. Mitul Hossen <143118872+mitulhossen@users.noreply.github.com>
2023-12-19 23:31:23 +0600

    Update index.blade.php

--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -17,7 +17,7 @@
                             <div class="col-12">
                                 <div class="d-flex align-items-lg-center flex-lg-row flex-column">
                                     <div class="flex-grow-1">
-                                        <h4 class="fs-16 mb-1">Good Morning, Ashik!</h4>
+                                        <h4 class="fs-16 mb-1">Good Morning, Mitul!</h4>
                                         <p class="text-muted mb-0">Here's what's happening with your store
                                             today.</p>
                                     </div>
